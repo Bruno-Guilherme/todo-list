27cc16c574925ac9fd79de28c7ea2aa9e342b234
Bruno Guilherme <dasilvaguilhermebruno@gmail.com>
2023-11-10 17:29:14 -0300

    feat: mudança no input e readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# todo-list
+# ToDo List
+Projeto desenvolvido para aplicar as habilidades adquiridas no curso introdutório de desenvolvimento web.
+
+![Imagem](#)
+
+### **Live Preview** do projeto [aqui]([url](https://bruno-guilherme.github.io/todo-list/))
+
+
+## Habilidades
+* HTML5
+* CSS3
+* JavaScript
+
+### Outros projetos
+* [Próximo](url)
--- script.js
+++ script.js
@@ -8,16 +8,20 @@ const criarTarefa = () => {
   const check = document.createElement("input");
   const tarefaOut = document.createElement("p");
 
-  check.setAttribute("type", "checkbox") 
-  div.appendChild(check)
-  tarefaOut.innerHTML = tarefaIn.value
-  div.appendChild(tarefaOut)
+  if (tarefaIn.value != "") {
+    check.setAttribute("type", "checkbox") 
+    div.appendChild(check)
+    tarefaOut.innerHTML = tarefaIn.value
+    div.appendChild(tarefaOut)
+  
+  
+    section.appendChild(div)
+  } else {
+    alert("Digite uma tarefa para salvar.")
+
+  }
 
 
-  section.appendChild(div)
 
-
-
-  console.log("Foi")
 };
 botaoAdd.addEventListener("click", criarTarefa);
