ec0a153cf7010cc5e2e6d0dc8ff84bd6419067c4
Bruno Guilherme <dasilvaguilhermebruno@gmail.com>
2023-11-11 16:04:53 -0300

    Updates

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ const botaoAdd = document.querySelector("button");
 
 const criarTarefa = () => {
   const tarefaIn = document.querySelector('#tarefa');
-  const section = document.querySelector('section')
-
-  const div = document.createElement("div");
-  const check = document.createElement("input");
-  const tarefaOut = document.createElement("p");
-
-  if (tarefaIn.value != "") {
-    check.setAttribute("type", "checkbox") 
-    div.appendChild(check)
-    tarefaOut.innerHTML = tarefaIn.value
-    div.appendChild(tarefaOut)
   
+  if (tarefaIn.value != "") {
+    const section = document.querySelector('section');
+    const div = document.createElement("div");
+    const check = document.createElement("input");
+    const tarefaOut = document.createElement("p");
+    const editar = document.createElement("button");
+    const deletar = document.createElement('button');
+
+    check.setAttribute("style", "type: checkbox; name: check");
+    check.type = "checkbox";
+    check.name = "check";
+    tarefaOut.innerHTML = tarefaIn.value;
+    editar.innerText = "Editar";
+    editar.setAttribute("type", "button");
+    deletar.innerText = "Deletar";
+    deletar.setAttribute("type", "button");
+
+    div.appendChild(check)
+    div.appendChild(tarefaOut)
+    div.appendChild(editar)
+    div.appendChild(deletar)
   
     section.appendChild(div)
   } else {
